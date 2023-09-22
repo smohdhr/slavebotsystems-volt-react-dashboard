76a78496587b0dfd3dc7f461d943a288ae7987b9
Mohammed Rizvi <smohdhr@hotmail.com>
2023-09-21 20:33:43 -0400

    after fixing the express static bug we can remove the coded route generation needed for debugging

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import path from 'path';
-//import fs from 'fs';
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { Routes } from '../src/routes.js';
 import express from 'express';
+//import fs from 'fs';
+//import { Routes } from '../src/routes.js';
 //import dns from 'dns';
 
 import Main from '../src/main.js';
@@ -24,16 +24,16 @@ function reactHandler(req, res) {
   }
 }
 
-function createReactRouting(app) {
-  for (const [key, value] of Object.entries(Routes)) {
-    let path = value.path;
-    app.get(path, reactHandler);
-  }
-}
+//function createReactRouting(app) {
+//  for (const [key, value] of Object.entries(Routes)) {
+//    let path = value.path;
+//    app.get(path, reactHandler);
+//  }
+//}
 
 const app = express();
 
-createReactRouting(app);
+//createReactRouting(app);
 
 const static_public_path = path.join(__dirname, '..', 'public');
 const static_serverbuild_path = path.join(__dirname, '..', 'server-build');
@@ -41,6 +41,8 @@ const static_serverbuild_path = path.join(__dirname, '..', 'server-build');
 // midleware
 app.use('/public', express.static(static_public_path));
 app.use('/server-build', express.static(static_serverbuild_path));
+
+app.get('*', reactHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${HOST} at port ${PORT}`);
