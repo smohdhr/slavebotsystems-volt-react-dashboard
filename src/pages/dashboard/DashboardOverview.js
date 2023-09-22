d7bc9e2732444473bdeb0e856102ef5dc1f1619c
Mohammed Rizvi <smohdhr@hotmail.com>
2023-09-21 22:48:12 -0400

    fortawsome node_module bug did not include styles for some reason

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
