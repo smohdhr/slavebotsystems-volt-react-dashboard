07f6c7d4bb3bf9df7de3ea0a698ee0349b2c3e71
Mohammed Rizvi <smohdhr@hotmail.com>
2023-10-02 18:15:51 -0400

    this fixes the module.export error on the client side

--- src/client.jsx
+++ src/client.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+var module = {
+  exports: {}
+};
+global.module = module;
+
 function hydrate() {
   let elem = document.getElementById("root");
   let loc = elem.getAttribute("location");
